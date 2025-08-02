e6610f2fa68524b622f1b4dc1768037972237911
tran3tin <tran3tin@gmail.com>
2025-08-02 22:42:38 +0700

    #30

--- src/main/java/com/example/demo/Controller/UserController.java
+++ src/main/java/com/example/demo/Controller/UserController.java
@@ -1,6 +1,9 @@
 package com.example.demo.Controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,5 +32,19 @@ public class UserController {
     public String deleteUser(@PathVariable("id") Long id) {
         this.userService.deleteUserById(id);
         return "id " + id + " da duoc xoa";
+    }
+
+    // Lấy thông tin user qua ID
+    @GetMapping("/user/{id}")
+    public User getUser(@PathVariable Long id) {
+        User user = this.userService.fetchUserById(id);
+        return user;
+    }
+
+    // Lấy thông tin toàn bộ User
+    @GetMapping("/user")
+    public List<User> getAllUsers() {
+        List<User> listUser = this.userService.fetchAllUsers();
+        return listUser;
     }
 }
--- src/main/java/com/example/demo/Service/UserService.java
+++ src/main/java/com/example/demo/Service/UserService.java
@@ -1,16 +1,22 @@
 package com.example.demo.Service;
 
-import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.stereotype.Service;
+import com.example.demo.Controller.UserController;
 import com.example.demo.Domain.User;
 import com.example.demo.Respository.UserRepository;
 
 @Service
 public class UserService {
+
+    private final Controller.UserController userController;
     private UserRepository userRepository;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, Controller.UserController userController) {
         this.userRepository = userRepository;
+        this.userController = userController;
     }
 
     public User createNewUser(User user) {
@@ -19,6 +25,18 @@ public class UserService {
 
     public void deleteUserById(Long id) {
         this.userRepository.deleteById(id);
+    }
+
+    public User fetchUserById(Long id) {
+        Optional<User> userOptional = this.userRepository.findById(id);
+        if (userOptional.isPresent()) {
+            return userOptional.get();
+        }
+        return null;
+    }
+
+    public List<User> fetchAllUsers() {
+        return this.userRepository.findAll();
     }
 
 }
